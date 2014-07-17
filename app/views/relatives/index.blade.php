3ac777b702d8be844cba1845bacc972d5eab29aa
ErichErnesto <ErichErnesto@10.0.33.93>
2014-07-17 11:55:21 -0500

    initial commit

--- app/views/relatives/index.blade.php
+++ app/views/relatives/index.blade.php
@@ -38,7 +38,42 @@
 		</tr>
 	</thead>
 	<tbody>
-	
+	@foreach($relatives as $key => $value)
+		<tr>
+                        <td>{{ $value->id }}</td>
+                        <td>{{ $value->abusedChild_id }}</td>
+                        <td>{{ $value->type }}</td>
+                        @if ($value->custodian)
+			<td>yes</td>
+                        @else
+                        <td>no</td>
+                        @endif
+                        @if ($value->sameHouse)
+			<td>yes</td>
+                        @else
+                        <td>no</td>
+                        @endif
+			<td>{{ $value->nameCalled }}</td>
+
+			<!-- we will also add show, edit, and delete buttons -->
+			<td>
+
+				<!-- delete the child (uses the destroy method DESTROY /children/{id} -->
+				<!-- we will add this later since its a little more complicated than the first two buttons -->
+				{{ Form::open(array('url' => 'relatives/' . $value->id, 'class' => 'pull-right')) }}
+					{{ Form::hidden('_method', 'DELETE') }}
+					{{ Form::submit('Delete this relatives', array('class' => 'btn btn-warning')) }}
+				{{ Form::close() }}
+
+				<!-- show the child (uses the show method found at GET /children/{id} -->
+				<a class="btn btn-small btn-success" href="{{ URL::to('relatives/' . $value->id) }}">Show this Relation</a>
+
+				<!-- edit this child (uses the edit method found at GET /children/{id}/edit -->
+				<a class="btn btn-small btn-info" href="{{ URL::to('relatives/' . $value->id . '/edit') }}">Edit this Relation</a>
+
+			</td>
+		</tr>
+	@endforeach
 	</tbody>
 </table>
 

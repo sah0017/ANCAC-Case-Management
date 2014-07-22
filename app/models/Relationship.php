2309f4adae81494fe95aec406b2b6b0a7f14b03b
ErichErnesto <ErichErnesto@10.0.33.93>
2014-07-22 15:12:14 -0500

    link relations to child and personal info

--- app/models/Relationship.php
+++ app/models/Relationship.php
@@ -2,4 +2,10 @@
 
 class Relationship extends \Eloquent {
 	protected $fillable = [];
+        public function personalInfo(){
+            return $this->belongsTo('person', 'person_id');   
+	}
+        public function child(){
+            return $this->belongsTo('abusedChild','abusedChild_id');
+	}
 }

adae95336616c9ac43355e417b83a0465159bae3
jbmur_000 <jbmur_000@Jonathan-pc>
2014-07-24 10:38:56 -0500

    edit problems with cases

--- app/controllers/CaseController.php
+++ app/controllers/CaseController.php
@@ -36,7 +36,69 @@ class CaseController extends \BaseController {
 	 */
 	public function store()
 	{
-		$case = new TrackedCase;
+		$person = new Person;
+		$person->name = Input::get('name',"");
+                $person->dob = Input::get('dob',"");
+                $person->drugUse = Input::get('drugUse',false);
+                $person->physicalAbuse = Input::get('physicalAbuse',false);
+                $person->sexAbuse = Input::get('sexAbuse',false);
+                $person->mentalHealthTreatment = Input::get('mentalHealthTreatment',false);
+                $person->crimeConviction = Input::get('crimeConviction',false);
+                $person->employed = Input::get('employed',false);
+                $person->fullTime = Input::get('fullTime',false);
+                $person->activeMilitary = Input::get('activeMilitary',false);
+                $person->sexAbuseSurvivor = Input::get('sexAbuseSurvivor',false);
+                $person->originCountry = Input::get('originCountry',"");
+                $person->specialNeeds = Input::get('specialNeeds',false);
+                $person->disability = Input::get('disability',false);
+                $person->language = Input::get('language',"");
+                $person->maritalStatus = Input::get('maritalStatus','single');
+                $person->address_id = Input::get('address_id',"");
+                $person->household_id = Input::get('household_id',"");
+                $person->ethnicity_id = Input::get('ethnicity_id',"");
+                $person->save();
+            
+                $child = new AbusedChild;
+                $child->person_id        = $person->id;
+		$child->parentalHistory  = Input::get('parentalHistory',"");
+		$child->parentStatus     = Input::get('parentStatus',"");
+                $child->medicalCompleted = Input::get('medicalCompleted',"");
+                $child->schoolGrade      = Input::get('schoolGrade',"");
+                $child->school           = Input::get('school',"");
+                $child->save();
+                
+                $person1 = new Person;
+		$person1->name = Input::get('name',"");
+                $person1->dob = Input::get('dob',"");
+                $person1->drugUse = Input::get('drugUse',false);
+                $person1->physicalAbuse = Input::get('physicalAbuse',false);
+                $person1->sexAbuse = Input::get('sexAbuse',false);
+                $person1->mentalHealthTreatment = Input::get('mentalHealthTreatment',false);
+                $person1->crimeConviction = Input::get('crimeConviction',false);
+                $person1->employed = Input::get('employed',false);
+                $person1->fullTime = Input::get('fullTime',false);
+                $person1->activeMilitary = Input::get('activeMilitary',false);
+                $person1->sexAbuseSurvivor = Input::get('sexAbuseSurvivor',false);
+                $person1->originCountry = Input::get('originCountry',"");
+                $person1->specialNeeds = Input::get('specialNeeds',false);
+                $person1->disability = Input::get('disability',false);
+                $person1->language = Input::get('language',"");
+                $person1->maritalStatus = Input::get('maritalStatus','single');
+                $person1->address_id = Input::get('address_id',"");
+                $person1->household_id = Input::get('household_id',"");
+                $person1->ethnicity_id = Input::get('ethnicity_id',"");
+                $person1->save();
+                        
+                        
+                $allegedAbuser = new AllegedAbuser;
+                $allegedAbuser->person_id        = $person1->id;
+                $allegedAbuser->known            = Input::get('known',false);
+                $allegedAbuser->adult            = Input::get('adult',false);
+        	$allegedAbuser->save();
+            
+                $case = new TrackedCase;
+                $case->abusedChild_id       = $child->id;
+                $case->allegedAbuser_id     =$allegedAbuser->id;
                 $case->abuse_id             = Input::get('abuse_id');
                 $case->worker_id            = Input::get('worker_id');
 		$case->abusedChild_id       = Input::get('abusedChild_id');

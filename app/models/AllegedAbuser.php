333c3f417daa320a504cebff6aba8c92b7bf7cf5
jeremy <jeremy@tankbook>
2014-07-24 09:22:10 -0500

    abuser model

--- app/models/AllegedAbuser.php
+++ app/models/AllegedAbuser.php
@@ -3,4 +3,13 @@
 class AllegedAbuser extends \Eloquent {
 	protected $fillable = [];
         protected $table = 'allegedAbusers';
+        
+        public function personalInfo(){
+            return $this->belongsTo('person', 'person_id');
+	}
+        
+        public function relation() {
+            return $this->hasMany('Relationship');
+        }
 }
+

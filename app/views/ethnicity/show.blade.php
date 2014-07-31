dd6e2156004bece7fd2672daec1fc9e8364b94b6
ErichErnesto <ErichErnesto@10.0.33.93>
2014-07-31 11:30:32 -0500

    Fixes to Ethnicity labels

--- app/views/ethnicity/show.blade.php
+++ app/views/ethnicity/show.blade.php
@@ -8,11 +8,11 @@
 
 <nav class="navbar navbar-inverse">
 	<div class="navbar-header">
-		<a class="navbar-brand" href="{{ URL::to('ethnicity') }}">Abuse Type Alert</a>
+		<a class="navbar-brand" href="{{ URL::to('ethnicity') }}">Ethnicity</a>
 	</div>
 	<ul class="nav navbar-nav">
-		<li><a href="{{ URL::to('ethnicity') }}">View All Abuse Types</a></li>
-		<li><a href="{{ URL::to('ethnicity/create') }}">Create a Abuse Type</a>
+		<li><a href="{{ URL::to('ethnicity') }}">View All Ethnicity</a></li>
+		<li><a href="{{ URL::to('ethnicity/create') }}">Create a Ethnicity</a>
 	</ul>
 </nav>
 

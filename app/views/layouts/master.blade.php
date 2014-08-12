cdac77ba1ef53092ce0dd0185752436dc59d6e99
ErichErnesto <ErichErnesto@172.17.8.17>
2014-08-12 09:27:05 -0500

    Home manue drop down box for links

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -37,6 +37,22 @@
                             <li><a href="{{{ URL::to('') }}}">Home</a></li>
                             @section('nav')
                             @show
+                            
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Preset Type <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{ URL::to('abuseTypes') }}">Abuse Types</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="{{ URL::to('ethnicity') }}">Ethnicity</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="{{ URL::to('relativeType') }}">Relative Type</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="{{ URL::to('serviceType') }}">Service Type</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="{{ URL::to('workerType') }}">Worker Type</a></li>
+                                    
+                                </ul>
+                            </li>
                     </div>
 
                     <div class="nav navbar-nav navbar-right">

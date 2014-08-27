52db60e2871009e9d5b3906a886f3ef443b7e5c2
ErichErnesto <ErichErnesto@10.0.6.39>
2014-08-27 12:24:27 -0500

    relative edit format

--- app/views/relatives/edit.blade.php
+++ app/views/relatives/edit.blade.php
@@ -6,7 +6,10 @@
 @stop
 
 @section('content')
-    
+
+<style>
+    #pad{padding: 8px}
+</style>
 <h1>Edit {{ $relative->personalInfo->name }}'s relation to {{ $relative->child->personalInfo->name }}</h1>
 {{ HTML::script('js/jquery-ui/jquery-ui.js') }}
 {{ HTML::style('js/jquery-ui/jquery-ui.css') }}
@@ -21,7 +24,7 @@
 
 
 {{ Form::model($relative, array('route' => array('relatives.update', $relative->id), 'method' => 'PUT')) }}
-
+<div class="form-inline"> 
 
         <div class="form-group">
 		{{ Form::label('gender', 'Gender') }}
@@ -32,23 +35,34 @@
 		{{ Form::label('relationType_id', 'Relation Type') }}
 		{{ Form::select('relationType_id', RelationType::where('center_id', Auth::User()->center_id)->orWhere('center_id', 99)->lists('type','id'), Input::old('relationType_id'), array('class' => 'form-control')) }}
 	</div>
-
-	<div class="form-group">
-		{{ Form::label('custodian', 'Is He/She the Custodion of the Child') }}
-		{{ Form::checkbox('custodian','1', Input::old('custodian'), array('class' => 'form-control')) }}
-	</div>
-
-	<div class="form-group">
-		{{ Form::label('sameHouse', 'Do they belong to the same house?') }}
-		{{ Form::checkbox('sameHouse', '1', Input::old('sameHouse'), array('class' => 'form-control')) }}
-	</div>
-
-	<div class="form-group">
+		<div class="form-group">
 		{{ Form::label('alias', 'Nickname of the person.') }}
 		{{ Form::text('alias', Input::old('alias'), array('class' => 'form-control')) }}
 	</div>
+</div>
+<br>
+<div class="form-horizontal">
+    <div class="checkbox">
+        <label>
 
 
+            {{ Form::checkbox('custodian','1', Input::old('custodian')) }}
+            <b>Is He/She the Custodion of the Child</b>
+        </label>
+    </div> 
+    
+ 
+    <div class="checkbox">
+        <label>
+
+
+            {{ Form::checkbox('sameHouse', '1', Input::old('sameHouse')) }}
+            <b>Do they belong to the same house?</b>
+        </label>
+    </div> 
+        
+</div>
+<br>
 
 	{{ Form::submit('Save', array('class' => 'btn btn-primary')) }}
 

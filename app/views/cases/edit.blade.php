78998b1c6319fbb9143d24138d787794b614d4e2
jbmur_000 <jbmur_000@Jonathan-pc>
2014-08-27 10:47:39 -0500

    change format of case create page

--- app/views/cases/edit.blade.php
+++ app/views/cases/edit.blade.php
@@ -26,7 +26,17 @@
 
 {{ Form::model($case, array('route' => array('cases.update', $case->id), 'method' => 'PUT')) }}
 
-         <div class="form-group">
+<div class="form-inline">
+    
+    <div style= 'padding: 8px' class="col-md-12">
+        <div class="form-group">
+		{{ Form::label('caseOpened', 'Case Opened') }}
+		{{ Form::text('caseOpened', date('Y-m-d'), array('class' => 'form-control')) }}
+	</div>
+    </div>
+    
+    <div style= 'padding: 8px' class="col-md-12">
+        <div class="form-group">
 		{{ Form::label('childFirst', 'Child\'s First Name') }}
 		{{ Form::text('childFirst', $case->abusedChild->personalInfo->first, array('class' => 'form-control')) }}
 	</div>
@@ -35,12 +45,14 @@
 		{{ Form::label('childMiddle', 'Middle Name') }}
 		{{ Form::text('childMiddle', $case->abusedChild->personalInfo->middle, array('class' => 'form-control')) }}
 	</div>
-
+    
         <div class="form-group">
 		{{ Form::label('childLast', 'Last Name') }}
 		{{ Form::text('childLast', $case->abusedChild->personalInfo->last, array('class' => 'form-control')) }}
 	</div>
-
+    </div>
+    
+    <div style= 'padding: 8px' class="col-md-12">
         <div class="form-group">
 		{{ Form::label('childAge', 'Child\'s Age') }}
 		{{ Form::text('childAge', $case->abusedChild->personalInfo->age, array('class' => 'form-control')) }}
@@ -51,105 +63,136 @@
 		{{ Form::select('gender', array('0' => 'Male', '1' => 'Female',), 
                     Input::old('gender'), array('class' => 'form-control')) }}
 	</div>
-
-        <div class="form-group">
-		{{ Form::label('worker_id', 'MDT Workers') }}
-		{{ Form::select('worker_id', Worker::where('center_id', Auth::User()->center_id)->lists('name','id'), Input::old('worker_id'), array('class' => 'form-control')) }}
-	</div>
-	
-        <div class="form-group">
-		{{ Form::label('note', 'Note') }}
-		{{ Form::text('note', Input::old('note'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
-		{{ Form::label('caseOpened', 'Case Opened') }}
-		{{ Form::text('caseOpened', date('Y-m-d'), array('class' => 'form-control')) }}
-	</div>
-
+        
         <div class="form-group">
 		{{ Form::label('county_id', 'County') }}
 		{{ Form::select('county_id', County::all()->lists('name','id'), Input::old('county_id'), array('class' => 'form-control')) }}
 	</div>
 
         <div class="form-group">
-		{{ Form::label('custodyIssues', 'Are there custody issues?') }}
-		{{ Form::checkbox('custodyIssues', '1', Input::old('custodyIssues'), array('class' => 'form-control')) }}
+		{{ Form::label('worker_id', 'Workers') }}
+		{{ Form::select('worker_id', Worker::where('center_id', Auth::User()->center_id)->lists('name','id'), Input::old('worker_id'), array('class' => 'form-control')) }}
 	</div>
-
-        <div class="form-group">
-		{{ Form::label('IOReport', 'Incident Offense Rpt') }}
-		{{ Form::checkbox('IOReport', '1', Input::old('IOReport'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
-		{{ Form::label('domesticViolence', 'Is There Domestic Violence') }}
-		{{ Form::checkbox('domesticViolence', '1', Input::old('domesticViolence'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
-		{{ Form::label('prosecution', 'Was DV Prosecuted') }}
-		{{ Form::checkbox('prosecution', '1', Input::old('prosecution'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
-		{{ Form::label('reporter', 'Reporter') }}
-		{{ Form::text('reporter', Input::old('reporter'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
-		{{ Form::label('abuseDate', 'Abuse Date') }}
-		{{ Form::text('abuseDate', Input::old('abuseDate'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
-		{{ Form::label('abuseLocation', 'Abuse Location') }}
-		{{ Form::text('abuseLocation', Input::old('abuseLocation'), array('class' => 'form-control')) }}
-	</div>
-
+    </div>
+    
+    <div style= 'padding: 8px' class="col-md-12">     
         <div class="form-group">
 		{{ Form::label('referralReason', 'Reason for Referral') }}
 		{{ Form::text('referralReason', Input::old('referralReason'), array('class' => 'form-control')) }}
 	</div>
-
+        
+         <div class="form-group">
+		{{ Form::label('agencyReportedTo', 'Reporting Agency') }}
+		{{ Form::text('agencyReportedTo', Input::old('agencyReportedTo'), array('class' => 'form-control')) }}
+	</div>
+        
+        <div class="form-group">
+		{{ Form::label('reportedDate', 'Reported Date') }}
+		{{ Form::text('reportedDate', Input::old('reportedDate'), array('class' => 'form-control')) }}
+	</div>
+    </div>
+    
+    <div style= 'padding: 8px' class="col-md-12"> 
+        <div class="form-group">
+		{{ Form::label('reporter', 'Reporter') }}
+		{{ Form::text('reporter', Input::old('reporter'), array('class' => 'form-control')) }}
+	</div>
+        
+        <div class="form-group">
+		{{ Form::label('abuseDate', 'Abuse Date') }}
+		{{ Form::text('abuseDate', Input::old('abuseDate'), array('class' => 'form-control')) }}
+	</div>
+        
+        <div class="form-group">
+		{{ Form::label('abuseLocation', 'Abuse Location') }}
+		{{ Form::text('abuseLocation', Input::old('abuseLocation'), array('class' => 'form-control')) }}
+	</div>
+    </div>
+    
+    <div style= 'padding: 8px' class="col-md-12">
         <div class="form-group">
 		{{ Form::label('DHRDetermination', 'DHR Determination') }}
 		{{ Form::select('DHRDetermination', array('0' => 'Select a Determination', '1' => 'Indicated',
                             '2' => 'Not Indicated', '3' => 'Unknown'), 
                     Input::old('DHRDetermination'), array('class' => 'form-control')) }}
-	</div>
-
+	</div> 
+        
         <div class="form-group">
-		{{ Form::label('forensicEvaluation', 'Extended Forensic Interview') }}
-		{{ Form::checkbox('forensicEvaluation', '1', Input::old('forensicEvaluation'), array('class' => 'form-control')) }}
-	</div>
-
+		{{ Form::label('chargesFiled', 'Charges Filed') }}
+		{{ Form::text('chargesFiled', Input::old('chargesFiled'), array('class' => 'form-control')) }}
+        </div>
+        
         <div class="form-group">
 		{{ Form::label('status', 'Status') }}
 		{{ Form::select('status', array('0' => 'Select Status', '1' => 'Open',
                             '2' => 'Closed'), 
                     Input::old('status'), array('class' => 'form-control')) }}
 	</div>
-
-        <div class="form-group">
-		{{ Form::label('chargesFiled', 'Charges Filed') }}
-		{{ Form::text('chargesFiled', Input::old('chargesFiled'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
-		{{ Form::label('agencyReportedTo', 'Reporting Agency') }}
-		{{ Form::text('agencyReportedTo', Input::old('agencyReportedTo'), array('class' => 'form-control')) }}
-	</div>
-
-        <div class="form-group">
+    </div>
+    
+    <div style= 'padding: 8px' class="col-md-6">
+        <div>
 		{{ Form::label('talkedToChild', 'Who has the child spoken with regarding the abuses.') }}
-		{{ Form::text('talkedToChild', Input::old('talkedToChild'), array('class' => 'form-control')) }}
+                <br>
+		{{ Form::textarea('talkedToChild', Input::old('talkedToChild'), array('class' => 'form-control')) }}
 	</div>
+    </div>
+    
+    <div style= 'padding: 8px' class="col-md-6">
+        <div>
+		{{ Form::label('note', 'Note') }}
+                <br>
+		{{ Form::textarea('note', Input::old('note'), array('class' => 'form-control')) }}
+	</div>
+    </div>
+</div>
 
-        <div class="form-group">
-		{{ Form::label('reportedDate', 'Reported Date') }}
-		{{ Form::text('reportedDate', Input::old('reportedDate'), array('class' => 'form-control')) }}
-	</div>
+<div class="form-horizontal">
+    <div class="checkbox">
+        <label>
+        		
+            {{ Form::checkbox('custodyIssues', '1', Input::old('custodyIssues')) }}
+            <b>Are there custody issues?</b>
+
+        </label>
+    </div>
+    <div class="checkbox">
+        <label>
+                   
+            {{ Form::checkbox('IOReport', '1', Input::old('IOReport')) }}
+            <b>Incident Offense Rpt</b>
+            
+        </label>
+    </div>
+
+    <div class="checkbox">
+        <label>
+                    
+            {{ Form::checkbox('domesticViolence', '1', Input::old('domesticViolence')) }}
+            <b>Is There Domestic Violence?</b>
+            
+        </label>
+    </div>  
+
+    <div class="checkbox"> 
+        <label>
+                    
+            {{ Form::checkbox('prosecution', '1', Input::old('prosecution')) }}
+            <b>Was DV Prosecuted</b>
+            
+        </label>
+    </div>
+
+    <div class="checkbox">
+        <label>
+                    
+            {{ Form::checkbox('forensicEvaluation', '1', Input::old('forensicEvaluation')) }}
+            <b>Extended Forensic Interview</b>
+            
+        </label>
+    </div>
+
+</div>
 
 	{{ Form::submit('Save', array('class' => 'btn btn-primary')) }}
 

a6acd75274d1ba581949d20f774f69389c21fe11
ErichErnesto <ErichErnesto@10.0.6.39>
2014-08-27 13:09:10 -0500

    routes

--- app/routes.php
+++ app/routes.php
@@ -183,4 +183,9 @@ Route::get('cases/{id}/child/relations/{realtion_id}',function($id, $relation_id
 Route::resource('school', 'SchoolController');
 Route::resource('countryOrigen', 'CountryOrigenController');
 Route::resource('session.sessionNotes', 'SessionNotesController');
+
+Route::get('session/{id}/notes/create', function($id) {
+    Session::put('from','session/'.$id);
+		return View::make('session.sessionNotes.create')->with('session', $id);
+});
 });//end of users

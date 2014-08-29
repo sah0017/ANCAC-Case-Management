8c4a0b5628edc3dda471c8c028407ea2a45c5386
jbmur_000 <jbmur_000@Jonathan-pc>
2014-08-29 09:43:31 -0500

    deleted files not needed and reformated the edit on child

--- app/views/households/edit.blade.php
+++ app/views/households/edit.blade.php
@@ -7,38 +7,60 @@
 
 @section('content')
 
+<style>
+    #pad{padding: 8px}
+</style>
+
 <h1>Edit {{ $household->name }}</h1>
 
 <!-- if there are creation errors, they will show here -->
 {{ HTML::ul($errors->all()) }}
 
 {{ Form::model($household, array('route' => array('households.update', $household->id), 'method' => 'PUT')) }}
-
-	<div class="form-group">
+<div class="form-inline">
+	<div id="pad" class="form-group">
 		{{ Form::label('pets', 'Pets') }}
 		{{ Form::text('pets', Input::old('pets'), array('class' => 'form-control')) }}
 	</div>
+</div>
+<div class="form-horizontal">
 
-	<div class="form-group">
-		{{ Form::label('medicare', 'Medicare') }}
-		{{ Form::checkbox('medicare', '1', Input::old('medicare'), array('class' => 'form-control')) }}
-	</div>
+    <div class="checkbox">
+        <label>
+		
+		{{ Form::checkbox('medicare', '1', Input::old('medicare')) }}
+                <b>Medicare</b>
+           
+        </label>
+    </div>
+    
+    <div class="checkbox">
+        <label>
+	
+		{{ Form::checkbox('allKids', '1', Input::old('allKids')) }}
+                <b>All Kids</b>
+                
+        </label>
+    </div>
+    
+    <div class="checkbox">
+        <label>
+	
+		{{ Form::checkbox('freeOrReducedLunch', '1', Input::old('freeOrReducedLunch')) }}
+                <b>Free or Reduced Lunch</b>
+        </label>	
+    </div>
 
-	<div class="form-group">
-		{{ Form::label('allKids', 'All Kids') }}
-		{{ Form::checkbox('allKids', '1', Input::old('allKids'), array('class' => 'form-control')) }}
-	</div>
-
-	<div class="form-group">
-		{{ Form::label('freeOrReducedLunch', 'Free or Reduced Lunch') }}
-		{{ Form::checkbox('freeOrReducedLunch', '1', Input::old('freeOrReducedLunch'), array('class' => 'form-control')) }}
-	</div>
-
-	<div class="form-group">
-		{{ Form::label('onBase', 'On Base') }}
-		{{ Form::checkbox('onBase', '1', Input::old('onBase'), array('class' => 'form-control')) }}
-	</div>
-
+    <div class="checkbox">
+        <label>
+		
+		{{ Form::checkbox('onBase', '1', Input::old('onBase')) }}
+                <b>On Base</b>
+    
+        </label>
+    </div>
+            
+</div>
 	{{ Form::submit('Save', array('class' => 'btn btn-primary')) }}
 
 {{ Form::close() }}

8bd03db2c2a2d0653364172bb7783aaf89c0ce92
jbmur_000 <jbmur_000@10.0.6.31>
2014-09-03 10:44:52 -0500

    spelling fix on dropdown  and reorder of date on all pages

--- app/views/cases/create.blade.php
+++ app/views/cases/create.blade.php
@@ -16,13 +16,13 @@
 {{ HTML::style('js/jquery-ui/jquery-ui.css') }}
 <script> 
     $(function() {
-    $( "#caseOpened" ).datepicker({ changeYear: true , yearRange: "c-60:c+60" , maxDate: "+0d",dateFormat: "yy-mm-dd" });
+    $( "#caseOpened" ).datepicker({ changeYear: true , yearRange: "c-60:c+60" , maxDate: "+0d",dateFormat: "mm-dd-yy" });
   });
   $(function() {
-    $( "#abuseDate" ).datepicker({ changeYear: true , yearRange: "c-60:c+60" , maxDate: "+0d",dateFormat: "yy-mm-dd" });
+    $( "#abuseDate" ).datepicker({ changeYear: true , yearRange: "c-60:c+60" , maxDate: "+0d",dateFormat: "mm-dd-yy" });
   });
   $(function() {
-    $( "#reportedDate" ).datepicker({ changeYear: true , yearRange: "c-60:c+60" , maxDate: "+0d",dateFormat: "yy-mm-dd" });
+    $( "#reportedDate" ).datepicker({ changeYear: true , yearRange: "c-60:c+60" , maxDate: "+0d",dateFormat: "mm-dd-yy" });
   });
   
  </script>

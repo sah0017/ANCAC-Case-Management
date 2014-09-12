d975f7cd7947e9cb61824ab5f68862522ac98f2d
ErichErnesto <ErichErnesto@172.17.8.26>
2014-09-12 11:16:13 -0500

    new case on the seeder

--- app/database/seeds/CaseTableSeeder.php
+++ app/database/seeds/CaseTableSeeder.php
@@ -33,6 +33,34 @@ class CaseTableSeeder extends Seeder {
             'reportedDate'=>'2006-09-13'
             
         ));
+        
+        TrackedCase::create(array(
+            'id' => '2',
+            'abusedChild_id'=>'2',
+            'caseNumber'=>'225',
+            'center_id'=>'1',
+            'worker_id'=>'4',
+            'note'=>'note of case 2',
+            'caseOpened'=>'2006-05-25',
+            'caseClosed'=>'2008-12-31',
+            'county_id'=>'1',
+            'custodyIssues'=>false,
+            'IOReport'=>false,
+            'domesticViolence'=>true,
+            'prosecution'=>true,
+            'reporter'=>'William Petterson',
+            'abuseDate'=>'2006-07-14',
+            'abuseLocation'=>'Down Town',
+            'referralReason'=>'Sexual Abuse',
+            'DHRDetermination'=>'indicated',
+            'forensicEvaluation'=>true,
+            'status'=>'open',
+            'chargesFiled'=>'charges filed',
+            'agencyReportedTo'=>'Faukner',
+            'talkedToChild'=>'James Smith',
+            'reportedDate'=>'2006-09-13'
+            
+        ));
        
     }
 }
--- app/database/seeds/PersonTableSeeder.php
+++ app/database/seeds/PersonTableSeeder.php
@@ -8,10 +8,22 @@ class PersonTableSeeder extends Seeder {
 
         Person::create(array(
             'id' => '1',
-            'first'=>'pepe',
+            'first'=>'Pepe',
             'last'=>'Smith',
             'dob'=>'1991-06-17',
             'age'=>'12',
+            'gender'=>'male',
+            'household_id' => 1,
+            'address_id' => 1
+            
+        ));
+        
+        Person::create(array(
+            'id' => '2',
+            'first'=>'Bill',
+            'last'=>'Gates',
+            'dob'=>'1997-06-17',
+            'age'=>'17',
             'gender'=>'male',
             'household_id' => 1,
             'address_id' => 1

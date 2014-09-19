9e3a85e7d2784808619de8191e7f06350bfd7d8c
jeremy <jeremy@tankbook>
2014-09-19 12:03:25 -0500

    initial files for MDT meetings

---
+++ app/database/migrations/2014_09_19_155736_create_MDTMeetings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMDTMeetingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('MDTMeetings', function(Blueprint $table)
+		{
+			$table->increments('id');
+                        $table->date('date');
+                        $table->text('location')->nullable();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('MDTMeeting');
+	}
+
+}
---
+++ app/models/MDTMeeting.php
@@ -0,0 +1,5 @@
+<?php
+
+class MDTMeeting extends \Eloquent {
+	protected $fillable = [];
+}

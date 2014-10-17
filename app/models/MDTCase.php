6a475d35a36a92105d0e05c5e06bf39a15df30fa
jeremy <jeremy@tankbook>
2014-10-17 09:50:31 -0500

    define tables in model

--- app/models/MDTCase.php
+++ app/models/MDTCase.php
@@ -2,6 +2,7 @@
 
 class MDTCase extends \Eloquent {
 	protected $fillable = [];
+        protected $table = "MDTCases";
         
         public function meeting(){
             return $this->belongsTo('MDTMeeting', 'MDTMeeting_id');
--- app/models/MDTMeeting.php
+++ app/models/MDTMeeting.php
@@ -2,6 +2,7 @@
 
 class MDTMeeting extends \Eloquent {
 	protected $fillable = [];
+        protected $table = "MDTMeetings";
         
         public function cases(){
             return $this->hasMany('MDTCase','MTDMeeting_id');
